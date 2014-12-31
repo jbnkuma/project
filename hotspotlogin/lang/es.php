181adde17c11f8129d8f1601c6a015a00184d7ff
jbnkuma <jesusbn5@gmail.com>
2014-12-30 19:27:52 -0600

    Se inicia la revision del proyecto para modificar el inicio de sesion para la red de PC SERVICE.

---
+++ hotspotlogin/lang/es.php
@@ -0,0 +1,23 @@
+<?php
+# daloRADIUS edition - fixed up variable definition through-out the code
+# as well as parted the code for the sake of modularity and ability to 
+# to support templates and languages easier.
+# Copyright (C) Enginx and Liran Tal 2007, 2008
+
+$ChilliSpot="PC Service wireless";
+$title="$ChilliSpot Iniciar Sesión";
+$centerUsername="Usuario";
+$centerPassword="Contraseña";
+$centerLogin="Iniciar Sesión";
+$centerPleasewait="Espera un momento.......";
+$centerLogout="Salir de la sesión";
+$h1Login="$ChilliSpot Iniciar Sesión";
+$h1Failed="$ChilliSpot Inicio de sesión  Fallido";
+$h1Loggedin="Conectado a $ChilliSpot";
+$h1Loggingin="Inicio de sesión en $ChilliSpot";
+$h1Loggedout="Desconectado de $ChilliSpot";
+$centerdaemon="Iniciar sesión debe realizarse a través de $ChilliSpot server";
+$centerencrypted="Nombre de usuario debe usar una conexión cifrada";
+
+
+?>
---
+++ hotspotlogin/lang/main.php
@@ -0,0 +1,14 @@
+<?php
+
+	$lang = "es";
+	switch($lang) {
+	
+		case "en":
+			include ("lang/es.php");
+			break;
+		default:
+			include ("lang/es.php");
+			break;
+	}
+
+?>
